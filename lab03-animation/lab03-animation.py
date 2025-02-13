c25e7e088dbad4106bc91231f4d5d04aed1534b1
AlanGISI <alan.czyz@edu.uah.es>
2025-02-13 20:11:06 +0100

    final piramide

--- lab03-animation/lab03-animation.py
+++ lab03-animation/lab03-animation.py
@@ -3,7 +3,7 @@ import arcade
 
 #--- codigo que genera el dibujo en sí
 def piramide():
-    arcade.draw_triangle_filled(300,5,780,5,540,300,arcade.color.AMBER)
+    arcade.draw_triangle_filled(300,40,780,40,540,450,arcade.color.AMBER)
 
 def sol():
     arcade.draw_circle_filled(0,720,200,arcade.color.AUREOLIN)
@@ -12,12 +12,16 @@ def fondo():
     arcade.set_background_color(arcade.color.BLACK)
 
 def arena():
-    arcade.draw_rectangle_filled(540,5,1090,10,)
+    arcade.draw_rectangle_filled(540,30,1090,60,arcade.color.ARYLIDE_YELLOW)
 
 def main():
     arcade.open_window(1080, 720, "Dibujo de la más alta calidad 2 :D")
     fondo()
+    #elementos a partir de aquí
     arcade.start_render()
-#-----
+    arena()
+    sol()
+    piramide()
     arcade.finish_render()
     arcade.run()
+main()
